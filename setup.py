cf2a1fae0ab2eefd72f9869b6324d782c998038b
James Moore <hello@jmoore.me>
2018-03-06 18:51:51 -0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,6 @@
+1 from setuptools import setup
+2 
+3 setup(
+4     name='RFExplorer',    # This is the name of your PyPI-package.
+5     version='1.20.1707.2',                          # Update the version number for new releases
+7 )
