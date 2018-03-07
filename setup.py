b5ff1c98c4b373e0e2e4c10a92bef7097c50c39f
James Moore <hello@jmoore.me>
2018-03-06 18:57:20 -0800

    fixed?

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
-1 from setuptools import setup
-2 
-3 setup(
-4     name='RFExplorer',    # This is the name of your PyPI-package.
-5     version='1.20.1707.2',                          # Update the version number for new releases
-7 )
+from setuptools import setup
+
+setup(
+    name='RFExplorer',    # This is the name of your PyPI-package.
+    version='1.20.1707.2',                          # Update the version number for new releases
+    install_requires=[
+        "pyserial",
+    ],
+)
