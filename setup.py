3368d938d74cd2a03508a3e53f0f8149e52b1dd1
James Moore <james@panic.com>
2019-03-04 14:36:58 -0800

    need to specify which folder to install

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from setuptools import setup
 
 setup(
-    name='RFExplorer',    # This is the name of your PyPI-package.
-    version='1.26.1806.1',                          # Update the version number for new releases
+    name='RFExplorer',  # This is the name of your PyPI-package.
+    version='1.26.1806.1',  # Update the version number for new releases
     install_requires=[
         "pyserial",
     ],
+    packages=['RFExplorer'],
 )
