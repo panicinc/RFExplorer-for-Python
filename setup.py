5ff1a8f87a3bd829161551e14f1d2c88094f9ef0
James Moore <james@panic.com>
2019-03-04 14:21:43 -0800

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='RFExplorer',    # This is the name of your PyPI-package.
-    version='1.20.1707.2',                          # Update the version number for new releases
+    version='1.26.1806.1',                          # Update the version number for new releases
     install_requires=[
         "pyserial",
     ],
